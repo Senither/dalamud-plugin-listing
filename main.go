6efd0b87dd99c197e3cd938ae898d9a839937ed2
Alexis Tan <alexis@sen-dev.com>
2024-08-08 02:44:23 +0200

    Split up HTTP server entirely into its own package

--- main.go
+++ main.go
@@ -1,37 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-
-	"github.com/senither/dalamud-plugin-listing/renders"
+	"github.com/senither/dalamud-plugin-listing/http"
 )
 
 func main() {
-	http.HandleFunc("/", handleRequest)
-
-	fmt.Println("Server is running on port 8080")
-	http.ListenAndServe(":8080", nil)
-}
-
-func handleRequest(w http.ResponseWriter, r *http.Request) {
-	fmt.Print(r.Method + ":" + r.URL.Path + " from " + r.RemoteAddr)
-
-	if r.URL.Path != "/" {
-		fmt.Println("404 Not Found:", r.URL.Path)
-
-		renders.RenderError(w, r)
-		return
-	}
-
-	w.Header().Set("Server", "'; DROP TABLE servertypes; --")
-	w.Header().Set("X-Powered-By", "Nerd Rage and Caffeine")
-	w.Header().Set("X-Accepts", "text/html,application/json")
-
-	if r.Header.Get("Accept") == "application/json" {
-		renders.RenderJson(w, r)
-		return
-	}
-
-	renders.RenderHtml(w, r)
+	http.SetupServer()
 }
