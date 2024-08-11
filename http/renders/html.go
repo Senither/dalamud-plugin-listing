c87c21d7189593902887d37c1363e960e7a47dba
Alexis Tan <alexis@sen-dev.com>
2024-08-11 04:02:08 +0200

    Replace html template string with file SHA1 hash

--- http/renders/html.go
+++ http/renders/html.go
@@ -1,12 +1,19 @@
 package renders
 
 import (
+	"crypto/sha1"
+	"encoding/hex"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/senither/dalamud-plugin-listing/metrics"
+)
+
+var (
+	fileHash *string
 )
 
 func RenderHtml(w http.ResponseWriter, r *http.Request) {
@@ -21,5 +28,16 @@ func RenderHtml(w http.ResponseWriter, r *http.Request) {
 		log.Fatal(err)
 	}
 
-	fmt.Fprintf(w, string(content))
+	if fileHash == nil {
+		hash := createSHA1Hash(string(content))
+		fileHash = &hash
+	}
+
+	fmt.Fprint(w, strings.Replace(string(content), "@file-hash", *fileHash, 1))
+}
+
+func createSHA1Hash(input string) string {
+	hash := sha1.Sum([]byte(input))
+
+	return hex.EncodeToString(hash[:])
 }
