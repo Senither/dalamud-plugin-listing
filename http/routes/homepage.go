637f16ba64b72e406c822ca6b49756c3e52861ce
Alexis Tan <alexis@sen-dev.com>
2026-03-12 22:44:05 +0100

    Add checks for internal and private plugins in homepage UI

--- http/routes/homepage.go
+++ http/routes/homepage.go
@@ -13,12 +13,19 @@ func HomepageHtml(c fiber.Ctx) error {
 		return repositories[i].Name < repositories[j].Name
 	})
 
+	var privatePlugins fiber.Map = make(fiber.Map)
+	for _, repo := range repositories {
+		privatePlugins[repo.InternalName] = repo.RepositoryOrigin.IsPrivatePlugin != nil &&
+			*repo.RepositoryOrigin.IsPrivatePlugin
+	}
+
 	return c.Render("homepage", fiber.Map{
 		"RepositoryCount":      state.GetUrlsSize(),
 		"PluginsTotalCount":    state.GetRepositoriesSize(),
 		"PluginsInternalCount": state.GetInternalPluginSize(),
 		"PluginsSenitherCount": state.GetSenitherPluginSize(),
 		"Plugins":              repositories,
+		"IsPrivatePlugin":      privatePlugins,
 	}, "layouts/app")
 }
 
