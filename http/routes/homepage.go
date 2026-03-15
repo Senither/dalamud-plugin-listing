73fbbb1ab07b3823886385a356d9840bf2eeb4b6
Alexis Tan <alexis@sen-dev.com>
2026-03-16 00:38:59 +0100

    Add support for sorting plugin list

--- http/routes/homepage.go
+++ http/routes/homepage.go
@@ -24,9 +24,6 @@ func HomepageJson(c fiber.Ctx) error {
 
 func RenderPluginListComponent(c fiber.Ctx) error {
 	repositories := state.GetRepositories()
-	sort.Slice(repositories, func(i, j int) bool {
-		return repositories[i].Name < repositories[j].Name
-	})
 
 	var privatePlugins fiber.Map = make(fiber.Map)
 	for _, repo := range repositories {
@@ -34,8 +31,60 @@ func RenderPluginListComponent(c fiber.Ctx) error {
 			*repo.RepositoryOrigin.IsPrivatePlugin
 	}
 
+	sortRepositories(repositories, c.Query("sort"))
+
 	return c.Render("components/plugin-list", fiber.Map{
 		"Plugins":         repositories,
 		"IsPrivatePlugin": privatePlugins,
 	})
+}
+
+func sortRepositories(repositories []state.Repository, sortKey string) {
+	sort.SliceStable(repositories, func(i, j int) bool {
+		left := repositories[i]
+		right := repositories[j]
+
+		switch sortKey {
+		case "name-desc":
+			return left.Name > right.Name
+		case "downloads-asc":
+			return getValue(left.DownloadCount) > getValue(right.DownloadCount)
+		case "downloads-desc":
+			return getValue(left.DownloadCount) < getValue(right.DownloadCount)
+		case "recently-updated":
+			return getLastUpdated(left) > getLastUpdated(right)
+
+		default:
+			return left.Name < right.Name
+		}
+	})
+}
+
+func getValue(value any) int64 {
+	switch v := value.(type) {
+	case int:
+		return int64(v)
+	case int64:
+		return v
+	case float64:
+		return int64(v)
+	}
+
+	return 0
+}
+
+func getLastUpdated(repository state.Repository) int64 {
+	var value = int64(0)
+
+	if repository.LastUpdated != nil {
+		value = getValue(repository.LastUpdated)
+	} else if repository.LastUpdate != nil {
+		value = getValue(repository.LastUpdate)
+	}
+
+	if value < 1_000_000_000_000 {
+		value *= 1000
+	}
+
+	return value
 }
